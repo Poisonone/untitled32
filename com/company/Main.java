7d3d74db4fd3f7a0e518c31e4b5affccfff15614
Sasha Veselov <realcool2222@gmail.com>
2020-09-30 14:53:23 +0300

    Initial commit

---
+++ com/company/Main.java
@@ -0,0 +1,60 @@
+package com.company.lesson1;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	Integer a = 10;
+	Boolean b = false;
+	Character c = 'C';
+	Float d = 0.5f;
+	Double e = 1d;
+	Long f = 1l;
+	Short g = 10;
+
+	System.out.println(a.intValue());
+	System.out.println(b.booleanValue());
+	System.out.println(c.charValue());
+	System.out.println(d.floatValue());
+	System.out.println(e.doubleValue());
+	System.out.println(f.longValue());
+	System.out.println(g.shortValue());
+	System.out.println(Integer.MAX_VALUE);
+	System.out.println(Integer.MIN_VALUE);
+	System.out.println(Float.MAX_VALUE);
+	System.out.println(Float.MIN_VALUE);
+	System.out.println(Double.MAX_VALUE);
+	System.out.println(Double.MIN_VALUE);
+	System.out.println(Long.MAX_VALUE);
+	System.out.println(Long.MIN_VALUE);
+	System.out.println(Character.MIN_VALUE);
+	System.out.println(Character.MAX_VALUE);
+	System.out.println(Short.MIN_VALUE);
+	System.out.println(Short.MAX_VALUE);
+	System.out.println(Byte.MAX_VALUE);
+	System.out.println(Byte.MIN_VALUE);
+		// ------------------------------------------------------ //
+		long min = 99999999999999999l;
+		int max = 0;
+		int ar[] = new int[10];
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < ar.length; i++) {
+			System.out.println("Vvedit ar[" + i + "]:");
+			int ms = sc.nextInt();
+			ar[i] = ms;
+		}
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Ar[" + i + "]=" + ar[i]);
+			if (max < ar[i]) {
+				max = ar[i];
+			}
+			if (min > ar[i]) {
+				min = ar[i];
+			}
+		}
+		System.out.println("Max:" + max);
+		System.out.println("Min:" + min);
+	}
+}
+
+
